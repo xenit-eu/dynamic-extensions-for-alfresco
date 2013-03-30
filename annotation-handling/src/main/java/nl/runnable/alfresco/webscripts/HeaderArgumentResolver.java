e6f941927862ada3292ddd639ae67dafd8bffb57
lfridael <laurens@runnable.nl>
2013-03-30 12:35:42 +0100

    Resolves issue #31.

---
+++ annotation-handling/src/main/java/nl/runnable/alfresco/webscripts/HeaderArgumentResolver.java
@@ -0,0 +1,29 @@
+package nl.runnable.alfresco.webscripts;
+
+import java.lang.annotation.Annotation;
+
+import nl.runnable.alfresco.webscripts.annotations.Header;
+
+import org.springframework.extensions.webscripts.WebScriptRequest;
+import org.springframework.extensions.webscripts.WebScriptResponse;
+
+/**
+ * {@link ArgumentResolver} that maps String parameters annotated with {@link Header}.
+ * 
+ * @author Laurens Fridael
+ * 
+ */
+public class HeaderArgumentResolver implements ArgumentResolver<String, Header> {
+
+	@Override
+	public boolean supports(final Class<?> argumentType, final Class<? extends Annotation> annotationType) {
+		return String.class.equals(argumentType) && Header.class.equals(annotationType);
+	}
+
+	@Override
+	public String resolveArgument(final Class<?> argumentType, final Header header, final String name,
+			final WebScriptRequest request, final WebScriptResponse response) {
+		return request.getHeader(header.value());
+	}
+
+}
---
+++ annotations/src/main/java/nl/runnable/alfresco/webscripts/annotations/Header.java
@@ -0,0 +1,25 @@
+package nl.runnable.alfresco.webscripts.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Maps an HTTP header in annotation-based Web Scripts to a handler method argument.
+ * <p>
+ * This annotation currently only works with String arguments.
+ * 
+ * @author Laurens Fridael
+ * 
+ */
+@Target({ ElementType.PARAMETER })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Header {
+
+	/**
+	 * The header name.
+	 */
+	String value();
+
+}
