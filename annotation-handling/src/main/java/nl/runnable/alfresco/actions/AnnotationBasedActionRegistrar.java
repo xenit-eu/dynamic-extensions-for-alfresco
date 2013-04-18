16f849b0d8ec0ba598e30bc594e67d56e15d91a7
Laurent Van der Linden <laurent.vanderlinden@gmail.com>
2013-04-18 22:08:32 +0200

    Register dummy actions to populate the action list.

--- annotation-handling/src/main/java/nl/runnable/alfresco/actions/AnnotationBasedActionRegistrar.java
+++ annotation-handling/src/main/java/nl/runnable/alfresco/actions/AnnotationBasedActionRegistrar.java
@@ -1,3 +1,29 @@
+/*
+Copyright (c) 2012, Runnable
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+ * Neither the name of Runnable nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package nl.runnable.alfresco.actions;
 
 import nl.runnable.alfresco.AbstractAnnotationBasedRegistrar;
@@ -5,6 +31,7 @@ import nl.runnable.alfresco.actions.annotations.ActionMethod;
 import nl.runnable.alfresco.actions.annotations.ActionParam;
 import org.alfresco.repo.action.ActionDefinitionImpl;
 import org.alfresco.repo.action.ParameterDefinitionImpl;
+import org.alfresco.repo.action.RuntimeActionService;
 import org.alfresco.repo.action.executer.ActionExecuter;
 import org.alfresco.service.cmr.action.Action;
 import org.alfresco.service.cmr.action.ActionDefinition;
@@ -48,6 +75,8 @@ public class AnnotationBasedActionRegistrar extends AbstractAnnotationBasedRegis
 
 	private ActionExecuterRegistry actionExecuterRegistry;
 
+	private RuntimeActionService runtimeActionService;
+
 	private final ParameterNameDiscoverer parameterNameDiscoverer = new LocalVariableTableParameterNameDiscoverer();
 
 	/* State */
@@ -67,13 +96,15 @@ public class AnnotationBasedActionRegistrar extends AbstractAnnotationBasedRegis
 					if (actionMethod != null) {
 						final ActionExecuter actionExecuter = createActionExecuter(beanFactory.getBean(beanName),
 								method, actionMethod);
-						final String name = actionExecuter.getActionDefinition().getName();
+						final ActionDefinition actionDefinition = actionExecuter.getActionDefinition();
+						final String name = actionDefinition.getName();
 						if (getActionExecuterRegistry().hasActionExecuter(name) == false) {
 							if (logger.isDebugEnabled()) {
 								logger.debug("Registering ActionExecuter {}.", name);
 							}
 							getActionExecuterRegistry().registerActionExecuter(actionExecuter);
 							registeredActionExecutors.add(actionExecuter);
+							registerActionToken(actionDefinition);
 						} else {
 							if (logger.isWarnEnabled()) {
 								logger.warn("ActionExecuter name has already been registered.");
@@ -83,6 +114,14 @@ public class AnnotationBasedActionRegistrar extends AbstractAnnotationBasedRegis
 				}
 			});
 		}
+	}
+
+	/**
+	 * Registry a dummy action so that the action list is extended.
+	 * It will not be executed unless the AnnotatedExecutor is uninstalled.
+	 */
+	private synchronized void registerActionToken(ActionDefinition actionDefinition) {
+		runtimeActionService.registerActionExecuter(new DummyActionExecutor(actionDefinition));
 	}
 
 	public void unregisterAnnotationBasedActions() {
@@ -260,4 +299,7 @@ public class AnnotationBasedActionRegistrar extends AbstractAnnotationBasedRegis
 		return actionExecuterRegistry;
 	}
 
+	public void setRuntimeActionService(RuntimeActionService runtimeActionService) {
+		this.runtimeActionService = runtimeActionService;
+	}
 }
