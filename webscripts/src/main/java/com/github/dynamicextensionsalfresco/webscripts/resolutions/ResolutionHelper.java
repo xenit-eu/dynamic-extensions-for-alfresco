85dff66a1206f05c51994c4754c53036ecfe3d31
Jonas Heylen <jonas.heylen@gmail.com>
2015-03-19 20:38:19 +0100

    ResolutionHelper.runAsAdmin: accept all Resolutions

--- webscripts/src/main/java/com/github/dynamicextensionsalfresco/webscripts/resolutions/ResolutionHelper.java
+++ webscripts/src/main/java/com/github/dynamicextensionsalfresco/webscripts/resolutions/ResolutionHelper.java
@@ -1,16 +1,18 @@
 package com.github.dynamicextensionsalfresco.webscripts.resolutions;
 
+import com.github.dynamicextensionsalfresco.webscripts.AnnotationWebScriptRequest;
+import com.github.dynamicextensionsalfresco.webscripts.AnnotationWebscriptResponse;
 import org.alfresco.repo.security.authentication.AuthenticationUtil;
 
 public class ResolutionHelper {
-	public static Resolution runAsAdmin(final AbstractResolution resolution) {
-		return new AbstractResolution() {
+	public static Resolution runAsAdmin(final Resolution resolution) {
+		return new Resolution() {
 			@Override
-			public void resolve() throws Exception {
+			public void resolve(final AnnotationWebScriptRequest request, final AnnotationWebscriptResponse response, final ResolutionParameters params) throws Exception {
 				AuthenticationUtil.runAsSystem(new AuthenticationUtil.RunAsWork<Object>() {
 					@Override
 					public Object doWork() throws Exception {
-						resolution.resolve();
+						resolution.resolve(request, response, params);
 						return null;
 					}
 				});
