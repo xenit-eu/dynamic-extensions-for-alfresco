578f45729de30401ee2333b1c0edcdc2a7216f50
Younes Regaieg <younes.regaieg@xenit.eu>
2017-06-13 16:52:35 +0200

    Prepare for next development iteration

--- gradle-plugin/src/main/java/com/github/dynamicextensionsalfresco/gradle/configuration/Versions.java
+++ gradle-plugin/src/main/java/com/github/dynamicextensionsalfresco/gradle/configuration/Versions.java
@@ -6,7 +6,7 @@ package com.github.dynamicextensionsalfresco.gradle.configuration;
  */
 
 public class Versions {
-	String dynamicExtensions = "1.6.1";
+	String dynamicExtensions = "1.6.2-SNAPSHOT";
 	
 	String surf = "5.0.d";
 
