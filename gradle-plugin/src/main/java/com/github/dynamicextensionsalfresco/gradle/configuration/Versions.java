8e9cb1787f9b218ca1ea472866bf75bdee8454f1
Stan Mine <stan.mine@xenit.eu>
2017-12-21 16:56:22 +0100

    release 1.7.3

--- gradle-plugin/src/main/java/com/github/dynamicextensionsalfresco/gradle/configuration/Versions.java
+++ gradle-plugin/src/main/java/com/github/dynamicextensionsalfresco/gradle/configuration/Versions.java
@@ -6,7 +6,7 @@ package com.github.dynamicextensionsalfresco.gradle.configuration;
  */
 
 public class Versions {
-	String dynamicExtensions = "1.7.3-SNAPSHOT";
+	String dynamicExtensions = "1.7.3";
 	
 	String surf = "5.0.d";
 
